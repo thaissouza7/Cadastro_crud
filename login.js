6e934af4bbd7a375e446c77b575d73468b5b8c51
Thais Souza <thaicxx9@gmail.com>
2023-07-14 16:36:43 -0300

    commit 2

--- login.js
+++ login.js
@@ -1,15 +1,17 @@
-document.getElementById("formularioLogin").addEventListener("submit", function(event) {
-    event.preventDefault();
 
+ 
+  document.getElementById("formularioLogin").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  
     var usuario = document.getElementById("usuarioGithub").value;
-      axios.get(`https://api.github.com/users/${usuario}`)
+    axios.get(`https://api.github.com/users/${usuario}`)
       .then(function(response) {
         var usuarioData = response.data;
-        localStorage.setItem("logins", JSON.stringify(usuarioData));
-        window.location.href = "index.html";
+        localStorage.setItem("logins", JSON.stringify(usuarioData)); // armazena os dados do usuário
+        window.location.href = "index.html"; // redireciona para a página "index.html"
       })
       .catch(function(error) {
-        alert("Usuário não encontrado no GitHub!");
+        alert("Usuário não encontrado no GitHub!"); // alerta caso não consiga encontrar o usuário
       });
   });
- 
+  
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function criarUsuario(event) {
     event.preventDefault();
 
+    // pega valores dos campos de entrada do formulário
     const nome = document.getElementById('nome').value;
     const sobrenome = document.getElementById('sobrenome').value;
     const email = document.getElementById('email').value;
@@ -14,6 +15,7 @@ function criarUsuario(event) {
     const cidade = document.getElementById('cidade').value;
     const estado = document.getElementById('estado').value;
 
+    //criacao do objeto com os valores captados
     const usuario = {
         nome: nome,
         sobrenome: sobrenome,
@@ -29,12 +31,15 @@ function criarUsuario(event) {
         habilidade: habilidade
     };
 
+   
     let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
+    // coloca o usuário ao array
     usuarios.push(usuario);
 
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
 
+    // limpa os campos do formulario
     document.getElementById('nome').value = '';
     document.getElementById('sobrenome').value = '';
     document.getElementById('email').value = '';
@@ -48,20 +53,23 @@ function criarUsuario(event) {
     document.getElementById('estado').value = '';
     document.getElementById('habilidade').value = '';
 
+
     exibirUsuarios();
 }
 
 function exibirUsuarios() {
+   
     const usuariosTable = document.getElementById('usuarios');
     usuariosTable.innerHTML = '';
-  
+
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
+    // estrutura da tabela de usuários
     const table = document.createElement('table');
     const thead = document.createElement('thead');
     table.appendChild(thead);
-  
 
+    // linha de cabeçalho da tabela
     const headerRow = document.createElement('tr');
     headerRow.innerHTML = `
       <th>Nome</th>
@@ -72,60 +80,68 @@ function exibirUsuarios() {
       <th>Ações</th>
     `;
     headerRow.style.backgroundColor = 'rgb(31, 30, 30)';
-
     thead.appendChild(headerRow);
 
     const tbody = document.createElement('tbody');
     table.appendChild(tbody);
-  
-    usuarios.forEach((usuario, index) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${usuario.nome} ${usuario.sobrenome}</td>
-        <td>${usuario.email}</td>
-        <td>${usuario.telefone}</td>
-        <td>${usuario.cidade}</td>
-        <td>${usuario.habilidade}</td>
-        <td>
-          <button class="editar">Editar</button>
-          <button class="excluir">Excluir</button>
-          <button class="visualizar">Visualizar</button>
-        </td>
-      `;
-    row.classList.add('tabelaUsuarios')
 
-      tbody.appendChild(row);
+    // preenche tabela com os dados dos usuários
+    usuarios.forEach((usuario, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${usuario.nome} ${usuario.sobrenome}</td>
+            <td>${usuario.email}</td>
+            <td>${usuario.telefone}</td>
+            <td>${usuario.cidade}</td>
+            <td>${usuario.habilidade}</td>
+            <td>
+                <button class="editar">Editar</button>
+                <button class="excluir">Excluir</button>
+                <button class="visualizar">Visualizar</button>
+            </td>
+        `;
+        row.classList.add('tabelaUsuarios');
+        tbody.appendChild(row);
     });
-  
+
+    // adiciona a tabela de usuários em html
     usuariosTable.appendChild(table);
-  
+
+   
     registrarEventosEdicao();
     registrarEventosExclusao();
     registrarEventosVisualizacao();
-  }
+}
 
 function visualizarUsuario(index) {
+   
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
+    
     const usuario = usuarios[index];
 
+    //  string de conteúdo com os detalhes do usuário
     const conteudo = `
-      Nome: ${usuario.nome} ${usuario.sobrenome}
-      Email: ${usuario.email}
-      Telefone: ${usuario.telefone}
-      CEP: ${usuario.habilidade}
-      Rua:${usuario.rua}
-      Número: ${usuario.numero}
-      Bairro:${usuario.bairro}
-      Cidade:${usuario.cidade}
-      Estado:  ${usuario.estado}
-      Habilidades: ${usuario.habilidade}
+        Nome: ${usuario.nome} ${usuario.sobrenome}
+        Email: ${usuario.email}
+        Telefone: ${usuario.telefone}
+        CEP: ${usuario.habilidade}
+        Rua:${usuario.rua}
+        Número: ${usuario.numero}
+        Bairro:${usuario.bairro}
+        Cidade:${usuario.cidade}
+        Estado:  ${usuario.estado}
+        Habilidades: ${usuario.habilidade}
     `;
+
+    // exibe um alerta com o conteúdo do usuário
     alert(conteudo);
 }
 
 function registrarEventosVisualizacao() {
+
     const botoesVisualizar = document.getElementsByClassName('visualizar');
+
 
     for (let i = 0; i < botoesVisualizar.length; i++) {
         const botaoVisualizar = botoesVisualizar[i];
@@ -140,6 +156,7 @@ function editarUsuario(index) {
 
     const usuario = usuarios[index];
 
+    // Preenche os campos do formulário com os dados do usuário
     document.getElementById('nome').value = usuario.nome;
     document.getElementById('sobrenome').value = usuario.sobrenome;
     document.getElementById('email').value = usuario.email;
@@ -148,30 +165,34 @@ function editarUsuario(index) {
     document.getElementById('habilidade').value = usuario.habilidade;
     document.getElementById('numero').value = usuario.numero;
 
-
     usuarios.splice(index, 1);
 
+    // atualiza o localStorage com o array de usuarios atualizado
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
-    buscarEndereco(usuario.cep);
 
+   
     exibirUsuarios();
 }
 
 function excluirUsuario(index) {
+  
     let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-
     usuarios.splice(index, 1);
 
+    // atualiza o localStorage com  todos os usuarios atualizados
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
 
+   
     exibirUsuarios();
 }
 
 async function buscarEndereco(cep) {
     try {
+        // faz uma requisição com base no cep digitad
         const response = await axios.get(`https://brasilapi.com.br/api/cep/v1/${cep}`);
         const endereco = response.data;
 
+        // preenche os campos do formulário 
         document.getElementById('rua').value = endereco.street || '';
         document.getElementById('bairro').value = endereco.neighborhood || '';
         document.getElementById('cidade').value = endereco.city || '';
@@ -183,7 +204,6 @@ async function buscarEndereco(cep) {
 
 function registrarEventosEdicao() {
     const botoesEditar = document.getElementsByClassName('editar');
-
     for (let i = 0; i < botoesEditar.length; i++) {
         const botaoEditar = botoesEditar[i];
         botaoEditar.addEventListener('click', function () {
@@ -193,6 +213,7 @@ function registrarEventosEdicao() {
 }
 
 function registrarEventosExclusao() {
+    //pega todos os botões de exclusão
     const botoesExcluir = document.getElementsByClassName('excluir');
 
     for (let i = 0; i < botoesExcluir.length; i++) {
@@ -204,14 +225,15 @@ function registrarEventosExclusao() {
 }
 
 document.getElementById('formulario').addEventListener('submit', criarUsuario);
-
 document.getElementById('cep').addEventListener('change', function (event) {
     const cep = event.target.value;
     buscarEndereco(cep);
 });
 
+// quando a pagina carregar, aparece os usuarios
 exibirUsuarios();
 
+// adiciona o nome e a foto do usuario do github
 document.addEventListener("DOMContentLoaded", function () {
     var logins = localStorage.getItem("logins");
     if (logins) {
@@ -221,8 +243,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+// exclui os dados do usuário e redireciona para a pagina de login
 var sair = document.getElementById("sair");
-
 sair.addEventListener("click", function () {
     localStorage.removeItem("logins");
     localStorage.removeItem("usuarios");
